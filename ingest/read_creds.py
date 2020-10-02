15a1de5c75a585bf441c5c237fec486c01930601
GLOrozcoM <glorozcom@protonmail.com>
2020-10-02 14:27:26 -0400

    Clean signatures and refactor

--- ingest/read_creds.py
+++ ingest/read_creds.py
@@ -4,28 +4,29 @@ Module to encapsulate reading credentials.
 - IP addresses
 """
 
+
 def read_db_creds(file_path):
     """ Read in creds from a file.
 
-    :param file_path:
-    :return:
+    :param file_path: String point to the file.
+    :return: A dictionary containing the role as key and role password as value.
     """
-    # TODO clean up
     # -- assume single line holds a role and password
-    properties = {}
+    role_creds = {}
     file = open(file_path, "r")
     line = file.readline()
     list_properties = line.strip().split(",")
-    properties[list_properties[0]] = list_properties[1]
-    properties[list_properties[2]] = list_properties[3]
+    role_creds[list_properties[0]] = list_properties[1]
+    role_creds[list_properties[2]] = list_properties[3]
     file.close()
-    return properties
+    return role_creds
+
 
 def read_ip_addresses(file_path):
     """ Read in IP addresses found at file_path.
 
-    :param file_path:
-    :return:
+    :param file_path: String point to the file.
+    :return: A dictionary containing role of machine as key and IP address as value.
     """
     ip_addresses = {}
     with open(file_path, "r") as file:
