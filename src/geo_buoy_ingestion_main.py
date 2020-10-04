18e32284910c0318f1c510cf5d4b9d5eeafdb415
GLOrozcoM <glorozcom@protonmail.com>
2020-10-04 14:10:48 -0400

    Transition from multiple tables per year to a single table per year.

--- src/geo_buoy_ingestion_main.py
+++ src/geo_buoy_ingestion_main.py
@@ -26,13 +26,18 @@ def extract_variables(h5_file):
     "s3://wpto-pds-us-wave/v1.0.0/virtual_buoy/US_virtual_buoy_" + year + ".h5"
     :return:
     """
-    # Extract actual data with [:] to perform calculation later
     energy_h5 = h5_file['energy_period'][:]
     swh_h5 = h5_file['significant_wave_height'][:]
+    omni_direct_pwr_h5 = h5_file['omni-directional_wave_power']
+    direct_coeff_h5 = h5_file['directionality_coefficient']
+    max_energy_direct_h5 = h5_file['maximum_energy_direction']
+    spectral_width_h5 = h5_file['spectral_width']
+
+    # Hourly, starting January 1st and going until December 31st
     time_h5 = h5_file['time_index']
     # Lat long format
     coord_h5 = h5_file['coordinates']
-    return energy_h5, swh_h5, time_h5, coord_h5
+    return energy_h5, swh_h5, time_h5, coord_h5, omni_direct_pwr_h5, direct_coeff_h5, max_energy_direct_h5, spectral_width_h5
 
 
 def calculate_power(swh_h5, energy_h5):
@@ -49,18 +54,13 @@ def calculate_power(swh_h5, energy_h5):
     return power_h5
 
 
-def convert_spark_df(power_h5, time_h5, coord_h5):
+def convert_coord_time_spark_df(time_h5, coord_h5):
     """ Convert h5 data to spark df.
 
-    :param power_h5: Power in h5 format.
     :param time_h5: Time index in h5 format.
     :param coord_h5: Coordinates in h5 format.
     :return: Data sets converted to spark data frames.
     """
-    print("Converting power to spark df")
-    power_sp = h5_to_pd_to_spark(power_h5)
-    print("Completed power conversion to spark df")
-
     print("Converting time to spark df")
     time_sp = h5_time_to_pd_to_spark(time_h5)
     time_sp = time_sp.withColumnRenamed("0", "time")
@@ -70,20 +70,36 @@ def convert_spark_df(power_h5, time_h5, coord_h5):
     coord_sp = h5_to_pd_to_spark(coord_h5)
     print("Completed coordinate conversion to spark df")
 
-    return power_sp, time_sp, coord_sp
+    return time_sp, coord_sp
 
 
-def write_to_db(db_name, power_geo_sp, year, j):
+def convert_metrics_spark_df(metric_list):
+    """ Convert all metrics to a spark df.
+
+    :param metric_list: A list with all metrics to convert to spark df.
+    :return:
+    """
+    N = len(metric_list)
+    converted_metrics = []
+    for i in range(0, N):
+        print("Converting {} to spark df".format(metric_list[i]))
+        single_metric = h5_to_pd_to_spark(metric_list[i])
+        print("Completed {} conversion to spark df".format(metric_list[i]))
+        converted_metrics.append(single_metric)
+    return converted_metrics
+
+
+def write_to_db(db_name, geo_metrics_sp, year, j):
     """ Write geographic buoy power data for a particular year to db.
 
     :param db_name: Data base name in Postgres to write to.
-    :param power_geo_sp: Geographic power data.
+    :param geo_metrics_sp: Geographic power data.
     :param year: Year ranging from 1979 to 2010.
     :param j: Location index for data set.
     :return: None
     """
     print("Started writing {}th geo tagged data set to db.".format(j))
-    write_to_postgres(db_name, power_geo_sp, "geo_power_" + year + "_loc" + str(j))
+    write_to_postgres(db_name, geo_metrics_sp, "geo_metrics_" + year)
     print("Completed writing geo data set to db.")
 
 
@@ -99,59 +115,103 @@ def access_lat_long(coords_sp_driver, j):
     return lat, long
 
 
-def give_id(power_sp, j):
+def give_id(metric_sp, j):
     """ Assign an id column to a selected power spark df.
 
-    :param power_sp: Spark df with power metric.
+    :param metric_sp: Spark df with power metric.
     :param j: Location index of coordinate; 57 distinct locations exist.
     :return: A power spark df with an id.
     """
-    single_column_power = power_sp.select(str(j))
-    single_column_power = single_column_power.withColumn("id_key", monotonically_increasing_id())
-    return single_column_power
+    single_column_metric = metric_sp.select(str(j))
+    single_column_metric = single_column_metric.withColumn("id_key", monotonically_increasing_id())
+    return single_column_metric
 
 
-def assign_coords(single_column_power, lat, long, j):
+def rename_metrics(metric_list, metric_names, location_index):
+    """ Give meaningful names to columns in metrics.
+
+    :param metric_list: A list containing metrics as spark data frames.
+    :param metric_names: Names for each metric. Order of metrics follows the metric list.
+    :param location_index: Location indexed in table to rename.
+    :return: Metric list with columns renamed.
+    """
+    N = len(metric_list)
+    meaningful_metrics = []
+    for i in range(0, N):
+        metric = metric_list[i].withColumnRenamed(str(location_index), metric_names[i])
+        meaningful_metrics.append(metric)
+    return meaningful_metrics
+
+
+def give_id_all_metrics(metric_list, j):
+    """ Assign an id to all metrics.
+
+    :param metric_list: List containing all metrics.
+    :param j: Location index of coordinate; 57 distinct locations exist.
+    :return: A list of metrics with id keys.
+    """
+    N = len(metric_list)
+    id_metrics = []
+    for i in range(0, N):
+        single_id_metric = give_id(metric_list[i], j)
+        id_metrics.append(single_id_metric)
+    return id_metrics
+
+
+def assign_coords(final_df, lat, long):
     """ Assign lat and long to a power column in spark.
 
-    :param single_column_power: Spark column with power.
+    :param final_df: Spark column with power.
     :param lat: Latitude column.
     :param long: Longitude column.
     :param j: Location index of coordinate; 57 distinct locations exist.
     :return: Lat and long coordinates combined with power.
     """
-    power_geo_sp = single_column_power.withColumn("lat", lit(lat))
-    power_geo_sp = power_geo_sp.withColumn("long", lit(long))
-    power_geo_sp = power_geo_sp.withColumnRenamed(str(j), "power")
-    return power_geo_sp
+    geo_df = final_df.withColumn("lat", lit(lat))
+    geo_df = geo_df.withColumn("long", lit(long))
+    return geo_df
 
 
-def make_location_datasets(coord_sp, power_sp, time_sp, year):
+def join_metrics(metric_list):
+    """ Join all metrics into one table.
+
+    :param metric_list: List containing metrics in spark df format.
+    :return: A single table with all metrics.
+    """
+    # Use first metric to begin join process
+    final_df = metric_list[0]
+    N = len(metric_list)
+    for i in range(1, N):
+        final_df = final_df.join(metric_list[i], on="id_key")
+    return final_df
+
+
+def make_location_datasets(coord_sp, metric_list, time_sp, year, metric_names):
     """ Given a year and relevant data sets, write out a power table for a single location.
 
-    :param j: Location index.
-    :param coord_sp: A spark data frame of lat long coordinstes.
-    :param power_sp: A spark data frame of power.
-    :param time_sp: A spark data frame of time.
+    :param metric_list: A list containing metrics to join together.
     :param year: A year ranging from 1979 to 2010.
     :return: None
     """
     print("Starting to create geo tagged data sets")
     coords_sp_driver = coord_sp.collect()
-    for j in range(0, 57):
+    for location_index in range(0, 57):
         begin_single_location = time.time()
-        print("Start creating a single location based data set.")
+        print("Start creating {}th location".format(location_index))
 
-        # Get lat longs and join with power data set
-        lat, long = access_lat_long(coords_sp_driver, j)
-        single_column_power = give_id(power_sp, j)
-        power_geo_sp = assign_coords(single_column_power, lat, long, j)
+        # Combine all metrics
+        id_metrics = give_id_all_metrics(metric_list, location_index)
+        named_metrics = rename_metrics(id_metrics, metric_names, location_index)
+        geo_metrics_sp = join_metrics(named_metrics)
+        lat, long = access_lat_long(coords_sp_driver, location_index)
+        geo_metrics_sp = assign_coords(geo_metrics_sp, lat, long)
+
         time_sp = time_sp.withColumn("id_key", monotonically_increasing_id())
-        power_geo_sp = power_geo_sp.join(time_sp, on="id_key")
+        geo_metrics_sp = geo_metrics_sp.join(time_sp, on="id_key")
 
         # Database writing
-        db_name = "geo_buoy_power"
-        write_to_db(db_name, power_geo_sp, year, j)
+        db_name = "geo_extracted_vars_buoy"
+        write_to_db(db_name, geo_metrics_sp, year, location_index)
 
         print("Finished creating a single location based data set.")
         end_single_location = time.time() - begin_single_location
@@ -169,12 +229,19 @@ if __name__ == "__main__":
 
         # Ingestion and processing
         h5_file = get_s3_data(year)
-        energy_h5, swh_h5, time_h5, coord_h5 = extract_variables(h5_file)
+        energy_h5, swh_h5, time_h5, coord_h5, omni_direct_pwr_h5, \
+        direct_coeff_h5, max_energy_direct_h5, spectral_width_h5 = extract_variables(h5_file)
+
         power_h5 = calculate_power(swh_h5, energy_h5)
-        power_sp, time_sp, coord_sp = convert_spark_df(power_h5, time_h5, coord_h5)
+        metric_list = [energy_h5, swh_h5, omni_direct_pwr_h5, direct_coeff_h5,
+                       max_energy_direct_h5, spectral_width_h5, power_h5]
+        metric_names = ["energy_period", "significant_wave_height", "omni_directional_power","directionality_coefficient",
+                        "maximum_energy_directionality", "spectral_width", "power"]
+        metrics_sp = convert_metrics_spark_df(metric_list)
+        time_sp, coord_sp = convert_coord_time_spark_df(time_h5, coord_h5)
 
         # Attach coordinates to individual locations and write individual tables to db
-        make_location_datasets(coord_sp, power_sp, time_sp, year)
+        make_location_datasets(coord_sp, metrics_sp, time_sp, year, metric_names)
 
         end_single_year = time.time() - begin_single_year
         print("A single year took {} seconds to process".format(end_single_year))
