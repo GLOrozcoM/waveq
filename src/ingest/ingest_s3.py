691663c93958b156e560b4f3e2211980fd838b5a
GLOrozcoM <glorozcom@protonmail.com>
2020-10-05 18:50:34 -0400

    Fix h5 deprecation warning.

--- src/ingest/ingest_s3.py
+++ src/ingest/ingest_s3.py
@@ -14,5 +14,5 @@ def call_s3_to_h5(s3_endpoint):
     # TODO does this need to be closed?
     # -- consider sending migrating opening to driver and closing file
     s3 = s3fs.S3FileSystem()
-    h5_file = h5py.File(s3.open(s3_endpoint,"rb"))
+    h5_file = h5py.File(s3.open(s3_endpoint,"rb"), "r")
     return h5_file
