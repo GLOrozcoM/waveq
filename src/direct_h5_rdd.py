00e1b4a9930542f8f30eb041f2a9d6d63b3e6167
GLOrozcoM <glorozcom@protonmail.com>
2020-10-07 14:29:57 -0400

    Distribution of read proof of concept - working version.

--- src/direct_h5_rdd.py
+++ src/direct_h5_rdd.py
@@ -7,9 +7,8 @@ Module to automate reading of h5 datasets into RDD format into Spark
 
 import h5py
 import sys
-import boto3
-from fs import open_fs
 import s3fs
+from ingest.ingest_s3 import *
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
@@ -20,35 +19,40 @@ partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 def get_s3_h5_files(csv_line):
     """ Use as a flat map function to distribute reading of a file.
 
-    :param csv_line: Encapsulates an s3 file path to an .h5 ocean wave file and internal dataset path.
+    :param csv_line: Encapsulates an s3 file path to an .h5 ocean wave file and internal data set path.
     :return:
     """
     lines = csv_line.split(",")
-
-    # BOTO3 attempts
-    # -- placeholder code, not expecting to run
-    s3_resource = 'settings'
-
-    bucket = s3_resource.Bucket("wpto-pds-us-wave")
-    obj = bucket.Object(key='v1.0.0/virtual_buoy/US_virtual_buoy_2010.h5')
-
-    # Time out occurring here
-    # -- access byte stream for reads
-    s3_file = obj.get()['Body'].read()
-
+    s3_endpoint = lines[0]
+    # -- check if not having three separate lines for a separate machine
+    s3 = s3fs.S3FileSystem(anon=True)
+    s3_file = s3.open(s3_endpoint, "rb")
     h5_file = h5py.File(s3_file, "r")
-    result = h5_file[lines[1]]
-    return list(result[:])
+
+    # Each h5_file has multiple data sets
+    # -- know for a fact 'coordinates' exists in file
+    dataset = h5_file['energy_period']
+    return list(dataset[()].tolist())
 
 print("Reading in to text file")
 file_paths = sc.textFile("h5_names_paths.csv", minPartitions=partitions)
 print("Finished reading in to text file")
 
+print(file_paths.take(3))
+
 print("Turning csv lines into rdd")
 rdd = file_paths.flatMap(get_s3_h5_files)
 print("Successfully turned csv lines into rdd")
 
+print(rdd.take(100))
+
 df = spark.createDataFrame(rdd)
-print(df)
+print("See the count of these data frames")
+print(df.count())
 
 sc.stop()
+
+#with h5py.File(s3_file, "r") as f:
+     #   with open("h5_keys.txt", "w") as log:
+      #      log.write("Data sets in h5 file: {}".format(f.keys()))
+       # result = f['energy_period']
