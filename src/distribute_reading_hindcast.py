a5e03f4f6cbe636ecb259ee7a24625302d825c3c
GLOrozcoM <glorozcom@protonmail.com>
2020-10-11 16:42:13 -0400

    Implement sounder indexing method for joins. First working version of distributed read for hindcast.

--- src/distribute_reading_hindcast.py
+++ src/distribute_reading_hindcast.py
@@ -1,6 +1,10 @@
 """
 Module to automate reading of h5 datasets into RDD format into Spark
 -- ensure h5_names_paths_one.csv is on worker machines as well as the driver.
+
+Entire process for 4 years takes 496 seconds.
+For a single data set write to db it is 70 seconds (max bound).
+Turning a single rdd to a df can take up to 20 seconds.
 """
 
 import h5py
@@ -12,7 +16,18 @@ from pyspark.sql import SparkSession
 import time
 import numpy as np
 import pandas as pd
-from pyspark.sql.functions import monotonically_increasing_id
+
+
+def h5_time_to_pd_to_spark(data_set):
+    """ Take in an h5 time stamped data set. Output a spark df.
+
+    :param data_set:
+    :return:
+    """
+    converted_time_np = [np.datetime64(entry) for entry in data_set]
+    time_pd = pd.DataFrame(converted_time_np)
+    sp_time = spark.createDataFrame(time_pd)
+    return sp_time
 
 
 def get_s3_hindcast(csv_line):
@@ -23,6 +38,7 @@ def get_s3_hindcast(csv_line):
     """
     lines = csv_line.split(",")
     s3_endpoint = lines[0]
+
     # The AWS repository is public so AWS creds are not needed
     s3 = s3fs.S3FileSystem(anon=True)
     s3_file = s3.open(s3_endpoint, "rb")
@@ -61,8 +77,11 @@ def create_metric_df(file_path):
     file_paths = read_s3_paths(file_path)
 
     print("Turning csv lines into rdd.")
+    lines_begin = time.time()
     rdd = file_paths.flatMap(get_s3_hindcast)
-    print("Successfully turned csv lines into rdd.")
+    end_lines = time.time() - lines_begin
+    print("Successfully turned csv lines into rdd in {} seconds.".format(end_lines))
+
 
     print("Going from RDD to data frame.")
     begin = time.time()
@@ -78,10 +97,10 @@ def h5_to_spark(data_set):
     :param data_set: An h5 data set.
     :return: A spark df.
     """
-    list_dataset = data_set.tolist()
-    rdd_dataset = sc.parallelize(list_dataset)
-    sp_dataset = rdd_dataset.toDF()
-    return sp_dataset
+    list_data_set = data_set.tolist()
+    rdd_data_set = sc.parallelize(list_data_set)
+    sp_data_set = rdd_data_set.toDF()
+    return sp_data_set
 
 
 def create_df_coords(s3_endpoint):
@@ -90,20 +109,22 @@ def create_df_coords(s3_endpoint):
     :param s3_endpoint:
     :return:
     """
-    # The AWS repository is public so AWS creds are not needed
     print("Getting coordinates")
+    begin = time.time()
+    # The AWS repository is public so AWS creds are not needed
     s3 = s3fs.S3FileSystem(anon=True)
     s3_file = s3.open(s3_endpoint, "rb")
     h5_file = h5py.File(s3_file, "r")
     coord_h5 = h5_file['coordinates']
     coord_df = h5_to_spark(coord_h5[:])
-    print("Completed getting coordinates")
+    end = time.time() - begin
+    print("Completed getting coordinates in {} seconds".format(end))
     s3_file.close()
     h5_file.close()
     return coord_df
 
 
-def turn_metrics_into_df(base_file_path):
+def turn_metrics_into_df(base_file_path, start_year, end_year):
     """ Turn selected wave metrics into spark df.
 
     NOTE time and coordinates are dealt with separately since their readings do not change
@@ -111,13 +132,14 @@ def turn_metrics_into_df(base_file_path):
 
     :return: A dictionary of spark df's.
     """
-    data_set_list = data_sets = ['directionality_coefficient', 'energy_period', 'maximum_energy_direction',
-                                 'omni-directional_wave_power', 'significant_wave_height', 'spectral_width']
+    # Notice 'omni-directional_wave_power' must have '-' otherwise won't be read in.
+    data_set_list = ['directionality_coefficient', 'energy_period', 'maximum_energy_direction',
+                      'omni-directional_wave_power', 'significant_wave_height', 'spectral_width']
     metric_df_dict = {}
     all_metrics_begin = time.time()
     print("Starting to get all metrics")
     for data_set in data_set_list:
-        file_path = base_file_path + data_set + "_1979_to_1982.csv"
+        file_path = base_file_path + data_set + "_" + str(start_year) + "_to_" + str(end_year) + ".csv"
         print("Getting data set {} at file path {}.".format(data_set, file_path))
         metric_df = create_metric_df(file_path)
         metric_df_dict[data_set] = metric_df
@@ -126,34 +148,106 @@ def turn_metrics_into_df(base_file_path):
     return metric_df_dict
 
 
-def generate_time_index(start_year, end_year):
-    """ Create a time index at a three hour resolution for given years.
-
+def get_time_index_hindcast(start_year):
+    """ Access time indices for data sets.
+    :param start_year: Year to start getting data for (min 1979, max 2006).
     :return: A spark df with time indices.
     """
-    print("Creating date times data frame.")
-    np_time = np.arange(np.datetime64(str(start_year) + '-01-01'), np.datetime64(str(end_year) + '-12-31'), np.timedelta64(3, 'h'))
-    pd_time = pd.DataFrame(np_time)
-    sp_time = spark.createDataFrame(pd_time)
-    print("Finished creating date times data frame.")
-    return sp_time
+    print("Accessing time data frames from s3.")
+    begin = time.time()
+
+    # Single starting year
+    s3_endpoint = "s3://wpto-pds-us-wave/v1.0.0/US_wave_" + str(start_year) + ".h5"
+    s3 = s3fs.S3FileSystem(anon=True)
+    s3_file = s3.open(s3_endpoint, "rb")
+    h5_file = h5py.File(s3_file, "r")
+    time_h5 = h5_file['time_index'][0:2918]
+    time_df = h5_time_to_pd_to_spark(time_h5)
+    s3_file.close()
+    h5_file.close()
+
+    # Combine years
+    for offset in range(1, 4):
+        s3_endpoint = "s3://wpto-pds-us-wave/v1.0.0/US_wave_" + str(start_year + offset) + ".h5"
+        s3 = s3fs.S3FileSystem(anon=True)
+        s3_file = s3.open(s3_endpoint, "rb")
+        h5_file = h5py.File(s3_file, "r")
+        time_h5 = h5_file['time_index'][0:2918]
+        new_time_df = h5_time_to_pd_to_spark(time_h5)
+        time_df = time_df.union(new_time_df)
+        s3_file.close()
+        h5_file.close()
+    end = time.time() - begin
+    print("Completed getting data frame from s3 in {} seconds.".format(end))
+    return time_df
 
 
 def give_id(metric_df):
-    """ Assign an id column to a selected power spark df.
+    """ Assign an id column to a spark df.
 
     :param metric_df: Spark df with power metric.
     :return: A power spark df with an id.
     """
-    metric_with_id = metric_df.withColumn("id_key", monotonically_increasing_id())
-    return metric_with_id
+    print("Starting to zip indices.")
+    begin_zip = time.time()
+    rdd_df = metric_df.rdd.zipWithIndex()
+    end_zip = time.time() - begin_zip
+    print("Completed zipping indices in {} seconds.".format(end_zip))
+
+    print("Going from zipped rdd to df.")
+    begin_df = time.time()
+    metric_df = rdd_df.toDF()
+    end_df = time.time() - begin_df
+    print("Completed going from zipped rdd to df in {} seconds.".format(end_df))
+
+    # Rename index
+    metric_df = metric_df.withColumnRenamed('_2', "id_key")
+
+    print("Starting to unpack columns from zipped format.")
+    unpack_begin = time.time()
+    # 1000 locations in original slice
+    for i in range(1, 1001):
+        # Note that '_1' doesn't change when accessing the columns
+        metric_df = metric_df.withColumn('loc_' + str(i), metric_df['_1'].getItem('_' + str(i)))
+    end_unpack = time.time() - unpack_begin
+    print("Finished unpacking columns from zipped format in {} seconds.".format(end_unpack))
+    # Crucial to not include column of columns
+    metric_df = metric_df.drop('_1')
+    return metric_df
+
+
+def give_id_time(metric_df):
+    """ Assign an id column to a spark df.
+
+    :param metric_df: Spark df with metric.
+    :return: A spark df with an id.
+    """
+    print("Starting to zip indices.")
+    begin_zip = time.time()
+    rdd_df = metric_df.rdd.zipWithIndex()
+    end_zip = time.time() - begin_zip
+    print("Completed zipping indices in {} seconds.".format(end_zip))
+
+    print("Going from zipped rdd to df.")
+    begin_df = time.time()
+    metric_df = rdd_df.toDF()
+    end_df = time.time() - begin_df
+    print("Completed going from zipped rdd to df in {} seconds.".format(end_df))
+
+    # Rename
+    metric_df = metric_df.withColumnRenamed('_2', "id_key")
+    metric_df = metric_df.withColumn('time', metric_df['_1'].getItem('time'))
+    metric_df = metric_df.drop('_1')
+
+    print("Metric looks like this: {}".format(metric_df.printSchema()))
+    return metric_df
 
 
 def give_all_metrics_id(metrics):
     """ Give all metrics an id to join with time.
 
-    :param metrics:
-    :return:
+    :param metrics: A dictionary containing spark data frames of ocean wave metrics.
+    :return: A dictionary containing same ocean wave metrics with an id.
     """
     metrics_with_id = {}
     for key in metrics:
@@ -161,10 +255,11 @@ def give_all_metrics_id(metrics):
     return metrics_with_id
 
 
-def join_time_to_metrics(metrics_with_id):
+def join_time_to_metrics(metrics_with_id, time_df):
     """ Give each wave metric a time index to query on.
 
-    :param metrics_with_id:
+    :param metrics_with_id: A dictionary of wave metrics with an id for joining.
+    :param time_df: A data frame containing time indices.
     :return: A dictionary containing time indexed metrics.
     """
     print("Starting to join all metrics with a time index.")
@@ -178,9 +273,17 @@ def join_time_to_metrics(metrics_with_id):
 
 
 def write_to_db(db_name, metrics_with_time_index, coordinates):
+    """ Write ocean wave metrics and coordinates to a data base.
+
+    :param db_name: Name of data base to write to.
+    :param metrics_with_time_index: A dictionary of ocean wave metrics, each data frame indexed by time.
+    :param coordinates: A data frame with lat and long columns.
+    :return: None.
+    """
     print("Writing coordinates to db.")
     write_to_postgres(db_name, coordinates, "coordinates")
     print("Writing coordinates ended.")
+    begin_all = time.time()
     for key in metrics_with_time_index:
         table_name = key
         data_frame = metrics_with_time_index[key]
@@ -189,11 +292,46 @@ def write_to_db(db_name, metrics_with_time_index, coordinates):
         write_to_postgres(db_name, data_frame, table_name)
         end_write = time.time() - begin_write
         print("Writing {} to postgres ended in {} seconds.".format(table_name, end_write))
+    end_all = time.time() - begin_all
+    print("Data base writing process took {} seconds to complete.".format(end_all))
+
+
+def run_four_year_block(start_year, end_year):
+    """ Encapsulate pipe line ingestion and processing for four years.
+
+    :param start_year: Year to start a four year block on. Minimum is 1979.
+    :param end_year: Year to start a four year block on. Max is 2010.
+    :return:
+    """
+    # Get relevant data frames
+    # - time indices get called to s3 directly on driver
+    time_df = get_time_index_hindcast(start_year)
+    time_df = time_df.withColumnRenamed("0", "time")
+    time_df = give_id_time(time_df)
+
+
+    # - take coordinates from 1979 since coordinates won't change across years
+    s3_endpoint = "s3://wpto-pds-us-wave/v1.0.0/US_wave_1979.h5"
+    coord_df = create_df_coords(s3_endpoint)
+    coord_df = coord_df.withColumnRenamed("_1", "latitude")
+    coord_df = coord_df.withColumnRenamed("_2", "longitude")
+
+    # - get six most important wave metrics
+    # - - significant_wave_height, omni_directional_wave_power, spectral_width,
+    # - - maximum_energy_direction, energy_period, directionality_coefficient
+    base_file_path = "hindcast_links/"
+    metrics = turn_metrics_into_df(base_file_path, start_year, end_year)
+    metrics_with_id = give_all_metrics_id(metrics)
+
+    # Operation
+    metrics_with_time_index = join_time_to_metrics(metrics_with_id, time_df)
+
+    # Write to postgresql
+    db_name = "hindcast"
+    write_to_db(db_name, metrics_with_time_index, coord_df)
 
 
 if __name__ == "__main__":
-    begin = time.time()
-
     # Spark setup
     sc = SparkContext(appName="Distribute reading of HDF5 files")
     spark = SparkSession(sc)
@@ -201,31 +339,12 @@ if __name__ == "__main__":
     # Reduce information printed on spark terminal
     sc.setLogLevel("ERROR")
 
-    ## High level operation -> acquire data frames of variables
-    # Manually generate time points here
+    begin = time.time()
     start_year = 1979
     end_year = 1982
-    time_df = generate_time_index(start_year, end_year)
-    time_df = give_id(time_df)
-
-    # Take coordinates from 1979 since coordinates won't change across years
-    s3_endpoint = "s3://wpto-pds-us-wave/v1.0.0/US_wave_1979.h5"
-    coord_df = create_df_coords(s3_endpoint)
-
-    # Get relevant wave metrics
-    base_file_path = "hindcast_links/"
-    metrics = turn_metrics_into_df(base_file_path)
-    metrics_with_id = give_all_metrics_id(metrics)
-
-    ## High level operation -> perform joins
-    # -- join each metric with time
-    metrics_with_time_index = join_time_to_metrics(metrics_with_id)
-
-    # Write to postgresql
-    db_name = "hindcast_test"
-    write_to_db(db_name, metrics_with_time_index, coord_df)
+    run_four_year_block(start_year, end_year)
+    end = time.time() - begin
+    print("Entire process took {} seconds.".format(end))
 
     sc.stop()
 
-    end = time.time() - begin
-    print("Entire process took {} seconds.".format(end))
