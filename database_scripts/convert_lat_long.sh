20da766c92d9022fdfaf4696eab82377d65edfbd
GLOrozcoM <glorozcom@protonmail.com>
2020-10-16 10:27:59 -0400

    Add user bash variable.

--- database_scripts/convert_lat_long.sh
+++ database_scripts/convert_lat_long.sh
@@ -3,13 +3,14 @@
 # -> Change to numeric to allow for precise and reliable location comparisons.
 
 # Designate your own tables and database
-table_name="base_name_"
-db_name="db_vars"
+table_name="wave_metrics_"
+db_name="buoy_single_table"
+user="user"
 
 # Years 1979 to 2010
 for i in {1979..2010}
 do
-psql -U -d $db_name -c "ALTER TABLE $table_name$i ALTER COLUMN lat TYPE numeric";
-psql -U -d $db_name -c "ALTER TABLE $table_name$i ALTER COLUMN long TYPE numeric";
+psql -U $user -d $db_name -c "ALTER TABLE $table_name$i ALTER COLUMN lat TYPE numeric";
+psql -U $user -d $db_name -c "ALTER TABLE $table_name$i ALTER COLUMN long TYPE numeric";
 done
 
